318d60add356fa96d98d4be2d736b015d2babc6c
MBadak <67018566+MBadak@users.noreply.github.com>
2023-04-28 18:35:08 +0200

    mr v1

--- src/AnaMenu.java
+++ src/AnaMenu.java
@@ -1,6 +1,6 @@
 public class AnaMenu {
     public static void main(String[] args) {
         System.out.println("Ana menü olusturunuz");
-
+        System.out.println("remote da yazildi. lokalden cagrildi");
     }
 }
