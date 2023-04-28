c87daefafaa80ec2de295498bcf581968ea5479b
Mustafa <m.badak99@gmail.com>
2023-04-28 18:54:56 +0200

    son

--- src/Araclar.java
+++ src/Araclar.java
@@ -1,5 +1,5 @@
 public class Araclar {
     public static void main(String[] args) {
-        System.out.println("Mustafa barnchindan olustu. ve buradan psuh edildi");
+        System.out.println("Mustafa barnchindan olustu. ve buradan psuh edildi.");
     }
 }
